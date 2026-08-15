282fd9f85c4e2790697e293d980753d9211ae3c8
syfssc-art <s.y.f.s.s.c@gmail.com>
2026-08-15 11:28:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME="adr-study-pwa-v1";
+const CACHE_NAME="adr-study-pwa-v2";
 const CORE=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE)));
